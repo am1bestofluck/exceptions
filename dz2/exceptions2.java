722efecf27f6280f98301cf76901b8826b0ea1a9
Anton <optimavar310am@gmail.com>
2023-06-28 15:21:37 +0300

    ui ready but why

---
+++ dz2/exceptions2.java
@@ -0,0 +1,11 @@
+package dz2;
+
+/**
+ * exceptions2
+ */
+public class exceptions2 {
+
+    public static void main(String[] args) {
+        Scanner main_scanner  = new Scanner (System.in);
+    }
+}
