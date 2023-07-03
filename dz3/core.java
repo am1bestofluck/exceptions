be106f4c35ae5d3d52372c6cadce8bbde9fe69b5
Anton <anton6733@gmail.com>
2023-07-04 02:08:29 +0300

    spr

--- dz3/core.java
+++ dz3/core.java
@@ -1,82 +1,78 @@
 package dz3;
+
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import dz3.exceptions.InputParseException;
 import dz3.exceptions.InputStructureException;
 
-
 import org.json.simple.JSONObject; // https://stackoverflow.com/questions/50232557/visual-studio-code-java-extension-how-to-add-a-jar-to-classpath
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 /**
  * 1. юзер запускает программу двойным кликом.()
  * 2. внутри core запускается repl
- *      2.1 repl принимает "add" и "qqq"
- *          2.2 "qqq" выходит из программы
- *          2.3 "add" начинает __РУТИНУ__
+ * 2.1 repl принимает "add" и "qqq"
+ * 2.2 "qqq" выходит из программы
+ * 2.3 "add" начинает __РУТИНУ__
  * 
- * 3.   __РУТИНА__(core)
- *      3.1 пока всё плохо: опрашиваем пользователя
- *              всё плохо = {в имени/фамилии цифры} или {в телефоне буквы}
- *      3.2 отдаём её в json_editor
+ * 3. __РУТИНА__(core)
+ * 3.1 пока всё плохо: опрашиваем пользователя
+ * всё плохо = {в имени/фамилии цифры} или {в телефоне буквы}
+ * 3.2 отдаём её в json_editor
  * 
- * 4.   делаем из строки json
+ * 4. делаем из строки json
  *
- * 5.   отдаём json в zip-editor # не отдаём ничего никуда. Впереди ад "контейнеризации", нужно 
- * с этим быстренько разобраться. 
+ * 5. отдаём json в zip-editor # не отдаём ничего никуда. Впереди ад
+ * "контейнеризации", нужно
+ * с этим быстренько разобраться.
  */
 
 public class core {
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         core a = new core();
         String[] validString = core.repl();
         System.out.println(validString);
         json_editor jse = new json_editor(validString);
-        jse.roll();
-   }
+        jse.getFile();
+    }
 
-   public static String[] repl(){
+    public static String[] repl() {
         Scanner localScanner = new Scanner(System.in);
-        String onlyDigits= "\\d+";
-        String noDigits ="\\D+";
+        String onlyDigits = "\\d+";
+        String noDigits = "\\D+";
         String temp = new String();
         String[] subStrings = new String[4];
-        
-        
-        while (true){
+
+        while (true) {
             System.out.println("Введи пож 'Фамилию' 'Имя' 'Отчество' 'Номер телефона' через пробелы");
             try {
                 temp = localScanner.nextLine();
                 subStrings = temp.split(" ");
-                if (subStrings.length != 4){
-                    throw new InputStructureException("через пробел <Ф> <И> <О> <телефон>"); 
+                if (subStrings.length != 4) {
+                    throw new InputStructureException("через пробел <Ф> <И> <О> <телефон>");
                 }
-                if (!subStrings[0].matches(noDigits)){
+                if (!subStrings[0].matches(noDigits)) {
                     throw new InputParseException("некорректный формат ФАМИЛИИ");
                 }
-                if (!subStrings[1].matches(noDigits)){
+                if (!subStrings[1].matches(noDigits)) {
                     throw new InputParseException("некорректный формат ИМЕНИ");
                 }
-                if (!subStrings[2].matches(noDigits)){
+                if (!subStrings[2].matches(noDigits)) {
                     throw new InputParseException("некорректный формат ОТЧЕСТВА");
                 }
-                if (!subStrings[3].matches(onlyDigits)){
+                if (!subStrings[3].matches(onlyDigits)) {
                     throw new InputParseException("некорректный формат НОМЕРА ТЕЛЕФОНА");
                 }
-                
+
                 return subStrings;
-            }
-            catch (InputStructureException | InputParseException  e){
-                System.out.println(e.getLocalizedMessage());}
-            catch (NoSuchElementException e) {
+            } catch (InputStructureException | InputParseException e) {
+                System.out.println(e.getLocalizedMessage());
+            } catch (NoSuchElementException e) {
                 System.out.println("Ну отказался так отказался :)");
             }
-            }
-            
         }
-        
-        }
-    
+
+    }
+
+}
--- dz3/json_editor.java
+++ dz3/json_editor.java
@@ -1,50 +1,61 @@
 package dz3;
 
 import java.nio.file.Path;
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+
 import java.io.File;
 import java.io.IOException;
+
 /**
  * json_editor
  */
 public class json_editor {
     String[] items;
-    public json_editor(String[] args){
-        this.items =args; 
+
+    public json_editor(String[] args) {
+        this.items = args;
     }
+
     public static void main(String[] args) {
         // принимаем массив строк;
-        String filename = args[0];
-        System.out.println();
         // если файл есть:
-            // читаем что есть в файле;
+        // читаем что есть в файле;
         // если файла нет:
-            // создаём пустой??
+        // создаём пустой??
         // дописываем;
     }
-    public void roll(){
+
+    public void roll() {
+        File chain = this.getFile();
+        this.appendFile(chain);
+    }
+
+    public File getFile() {
         String filename = this.items[0];
-        String relativePath = "./dz3/"+filename+".json";
-        if (!Path.of(relativePath).toFile().exists())
-            {
-                File newFile = new File(relativePath);
-                try
-                {
-                    newFile.createNewFile();
-                }
-                catch (IOException e){}
+        String relativePath = "./dz3/" + filename + ".json";
+        File newFile = null;
+        if (!Path.of(relativePath).toFile().exists()) {
+            newFile = new File(relativePath);
+            try {
+                newFile.createNewFile();
+            } catch (IOException e) {
+
             }
 
-        System.out.println();
-    }
-    private boolean fileCreator (String filename){
-        return true;
-    }
-    private String fileReader(String fileName){
-        String out = new String();
-        return out;
+        } else {
+            newFile = new File(relativePath);
+        }
+
+        return newFile;
     }
 
-    private void fileWriter(String arg){
-
+    private void appendFile(File arg) {
+        JSONObject new_content = new JSONObject(new HashMap<String, String>());
+        new_content.put("name", this.items[0]);
+        new_content.put("lastName", this.items[1]);
+        new_content.put("byFather", this.items[2]);
+        new_content.put("byFather", this.items[3]);
     }
 }
